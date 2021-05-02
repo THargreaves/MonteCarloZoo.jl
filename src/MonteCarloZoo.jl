4f21a7673f0823f4885f8113e41a14c7cbd9724b
Tim <tim.hargreaves@icloud.com>
2021-05-02 22:18:22 +0100

    Add rejection sampling

--- src/MonteCarloZoo.jl
+++ src/MonteCarloZoo.jl
@@ -1,5 +1,22 @@
-module MonteCarloZoo
+module RejectionSampler
 
-greet() = print("Hello World!")
+export rejectionSampler
 
-end # module
+function rejection_sampler(target_density, proposal_density, proposal_sampler,
+                           scale, iterations, dimension)
+    samples = Array{Float64}(undef, dimension, iterations)
+    
+    i = 1
+    while i <= iterations
+        proposal = proposal_sampler()
+        threshold = target_density(proposal) / (scale * proposal_density(proposal))
+        if rand() < threshold
+            samples[:, i] = proposal
+            i += 1
+        end
+    end
+
+    return samples
+end
+
+end  # module
---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using Test, MonteCarloZoo
+
+target_density(x) = exp(-x^2 / 2) / sqrt(2π)  # standard normal
+proposal_density(x) = (1 + x^2) / π  # Cauchy
+proposal_sampler() = tan(π * (rand() - 0.5))
+scale = sqrt(2π / e)
+iterations = 100
+dimension = 1
+
+samples = rejection_sampler(
+    target_density, proposal_density, proposal_sampler,
+    scale, iterations, dimension
+)
+
+@test size(samples) == (dimension, iterations)
