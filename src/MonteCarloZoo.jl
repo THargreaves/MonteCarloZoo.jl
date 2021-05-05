35c5a00a519ff06b1b3c7741261a26a82c733f09
Tim <tim.hargreaves@icloud.com>
2021-05-05 12:37:09 +0100

    Update package structure

--- src/MonteCarloZoo.jl
+++ src/MonteCarloZoo.jl
@@ -5,7 +5,12 @@ A broad collection of Monte Carlo algorithms implemented in Julia.
 """
 module MonteCarloZoo
 
-include("Sequential.jl")
+include("Diagnostics.jl")
+include("Filtering.jl")
+include("Generators.jl")
 include("MarkovChain.jl")
+include("Simple.jl")
+include("Transformation.jl")
+include("Utils.jl")
 
 end  # module
